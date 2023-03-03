fd41f441ad8c24bfdf8587e9194eeb66511dde51
Lukasz Jurkiewicz <wookash@Lukaszs-MacBook-Pro.local>
2023-03-03 23:30:32 +0000

    buttons in search history box can be used to search again for the forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,26 @@
 const APIKey = 'c0b6600a97a11c8918dfc87b9f7842c6';
 var cityNameSearch = $('#city-name-search');
 var historyContainerEl = $('#search-history');
-var historyContainerListEl = $('<ul>');
 var savedCities = new Array();
-var timeNow = dayjs().format("DD/MM/YYYY");
-// var btnStyle = $('<button type="button" class="btn btn-secondary m-1"></button>');
 
+// func at start of the app
 $(function() {
     if(JSON.parse(localStorage.getItem("cities") !== null)) savedCities = JSON.parse(localStorage.getItem("cities"));
     renderCityHistoryList();
-
-    console.log(savedCities);
 });
 
+// fetch data from localstorage and populate list of search history
+function renderCityHistoryList() {
+    for(var i=0; i<savedCities.length; i++){
+        var city = savedCities[i];
+        var btnEl = $('<button type="button" data-city="' + city + '" class="btn btn-secondary m-1"></button>');
+        btnEl.attr("data-city", city).text(city);
+        historyContainerEl.append(btnEl);
+    };
 
+};
 
+// add city to search history as a button, add city to localstorage
 function addCityToLocalStorage(city) {
     if(savedCities.includes(city)) return;
     savedCities.unshift(city);
@@ -24,45 +30,36 @@ function addCityToLocalStorage(city) {
     historyContainerEl.prepend(btnEl);
 };
 
-function renderCityHistoryList() {
-    for(var i=0; i<savedCities.length; i++){
-        var city = savedCities[i];
-        var btnEl = $('<button type="button" class="btn btn-secondary m-1"></button>');
-        btnEl.attr("data-city", city).text(city);
-        historyContainerEl.append(btnEl);
-    };
 
-};
 
+// --------- MAIN FUNCTION
 function showWeather(city) {
+    // connect to current weather API
     var currentWeatherURL = 
         'https://api.openweathermap.org/data/2.5/weather?q=' 
         + city + '&units=metric&appid=' + APIKey;
+    
+    // Connect to 5 days every 3 hours API
     var forecastURL = 
         'http://api.openweathermap.org/data/2.5/forecast?q=' 
         + city + '&units=metric&appid=' + APIKey;
 
     fetch(currentWeatherURL).then(function(response) {
-        if(response.ok) {
+        if(response.ok) { // if city exists (== connection success)
             response.json().then(function (data) {
-                console.log(response);
-                console.log(data);
                 addCityToLocalStorage(city);
+                console.log(data);
                 var icon = $('<img src="http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png">')
 
-                $('#today-city-name').text(data.name + " (" + timeNow + ")").append(icon);
+                $('#today-city-name').text(data.name + " (" + dayjs().format("DD/MM/YYYY") + ")").append(icon);
                 $('#today-temp').text(data.main.temp + " ℃");
                 $('#today-wind').text(data.wind.speed + " mph");
                 $('#today-hum').text(data.main.humidity + " %");
 
+                clearForecastEl(); // clear div
+
                 fetch(forecastURL).then(function(response) {
                     response.json().then(function(data) {
-                        console.log('---------------------');
-                        console.log(data);
-                        console.log(data.list[4].dt_txt.split(' ')[1].split(':')[0]);
-                        console.log(typeof parseInt(data.list[4].dt_txt.split(' ')[1].split(':')[0]));
-                        clearForecastEl();
-                        console.log(dayjs().add(1,'day'));
                         for(var i=0; i<40; i++) {
                             if(parseInt(data.list[i].dt_txt.split(' ')[1].split(':')[0]) === 12) {
                                 var cardEl = $('<div class="bg-secondary p-2 m-2 flex-fill">');
@@ -71,38 +68,37 @@ function showWeather(city) {
                                 var temp = $('<p>').text('Temp: ' + data.list[i].main.temp + " ℃");
                                 var wind = $('<p>').text('Wind: ' + data.list[i].wind.speed + ' mph');
                                 var hum = $('<p>').text('Humidity: ' + data.list[i].main.humidity + " %");
+
                                 $('#forecast').append(cardEl);
                                 cardEl.append(date).append(icon).append(temp).append(wind).append(hum); 
-                            };
-                                        
+                            };         
                         };
                     });
                 });
-
             });
         } else {
             alert('City does not exist.')
-        }
+        };
     });
 };
 
+// clear forecast section
+function clearForecastEl() {
+    const parent = $('#forecast');
+    parent.empty();
+};
 
+// button handlers
+cityNameSearch.on('submit', handleCitySearch);
+
+historyContainerEl.click(function(event) {
+    var temp = $(event.target);
+    showWeather(temp.data('city'));
+});
 
 function handleCitySearch(event) {
     event.preventDefault();
     city = $('input[name="city"').val();
     showWeather(city);
     cityNameSearch[0].reset(); // clear the value
-
 };
-function clearForecastEl() {
-    const parent = $('#forecast');
-    parent.empty();
-};
-cityNameSearch.on('submit', handleCitySearch);
-
-// 
-// buttons to work on the weather somehow
-// data atr set data-city to store city names in button element, i think i can use it for later
-
-// 5day forecast
